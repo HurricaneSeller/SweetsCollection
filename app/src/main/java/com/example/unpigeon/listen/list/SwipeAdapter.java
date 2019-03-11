9eb71c27072e880f6896d1985ef1ddfbc65da567
moanbigking <962336379@qq.com>
2019-03-11 14:31:49 +0800

    rename and delete waited

--- app/src/main/java/com/example/unpigeon/listen/list/SwipeAdapter.java
+++ app/src/main/java/com/example/unpigeon/listen/list/SwipeAdapter.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.unpigeon.R;
 import com.example.unpigeon.record.RecordActivity;
@@ -15,7 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder>{
     private List<RecordPiece> mRecordPieces;
-    public SwipeAdapter(List<RecordPiece> recordPieces) {
+    SwipeAdapter(List<RecordPiece> recordPieces) {
         mRecordPieces = recordPieces;
     }
     @NonNull
@@ -23,12 +25,16 @@ public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder>{
     public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, int viewType) {
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_item, parent, false);
         final SwipeAdapter.ViewHolder viewHolder =new SwipeAdapter.ViewHolder(view);
-        viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+        viewHolder.deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // with information
-                Intent intent = new Intent(parent.getContext(), RecordActivity.class);
-                parent.getContext().startActivity(intent);
+                // TODO: 3/11/19  send broadcast to remove the piece
+            }
+        });
+        viewHolder.renameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 3/11/19 send broadcast to rename the piece
             }
         });
         return viewHolder;
@@ -36,7 +42,9 @@ public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // TODO: 2/26/19
+        RecordPiece recordPiece = mRecordPieces.get(position);
+        String tmp = recordPiece.getName();
+        holder.summaryView.setText(tmp);
     }
 
     @Override
@@ -46,9 +54,15 @@ public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder>{
 
     class ViewHolder extends RecyclerView.ViewHolder{
         View mView;
+        Button renameButton;
+        Button deleteButton;
+        TextView summaryView;
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             mView = itemView;
+            renameButton = mView.findViewById(R.id.fra_item_rename);
+            deleteButton = mView.findViewById(R.id.fra_item_delete);
+            summaryView = mView.findViewById(R.id.fra_item_text);
         }
     }
 }
